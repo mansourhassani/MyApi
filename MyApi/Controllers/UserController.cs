385941e8155fcf112baccfa5f92f6712d8f37a8a
mansourhassani <mansour.hasani.dev@gmail.com>
2024-02-28 00:04:37 +0330

    Add Standard Api Result.

--- MyApi/Controllers/UserController.cs
+++ MyApi/Controllers/UserController.cs
@@ -4,6 +4,8 @@ using Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading;
+using WebFramework.Api;
+using WebFramework.Filters;
 
 namespace MyApi.Controllers
 {
@@ -19,27 +21,31 @@ namespace MyApi.Controllers
         }
 
         [HttpGet]
-        public async Task<List<User>> Get(CancellationToken cancellationToken)
+        [ApiResultFilter]
+        public async Task<ActionResult<List<User>>> Get(CancellationToken cancellationToken)
         {
             var users = await userRepository.TableNoTracking.ToListAsync(cancellationToken);
-            return users;
+            return Ok(users);
         }
 
         [HttpGet("{id:int}")]
-        public async Task<ActionResult<User>> Get(int id, CancellationToken cancellationToken)
+        public async Task<ApiResult<User>> Get(int id, CancellationToken cancellationToken)
         {
             var user = await userRepository.GetByIdAsync(cancellationToken, id);
+            if (user == null)
+                return NotFound();
             return user;
         }
 
         [HttpPost]
-        public async Task Create(User user, CancellationToken cancellationToken)
+        public async Task<ApiResult<User>> Create(User user, CancellationToken cancellationToken)
         {
             await userRepository.AddAsync(user, cancellationToken);
+            return Ok(user);
         }
 
         [HttpPut]
-        public async Task<ActionResult> Update(int id, User user, CancellationToken cancellationToken)
+        public async Task<ApiResult> Update(int id, User user, CancellationToken cancellationToken)
         {
             var updateUser = await userRepository.GetByIdAsync(cancellationToken, id);
 
@@ -52,12 +58,11 @@ namespace MyApi.Controllers
             updateUser.LastLoginDate = user.LastLoginDate;
 
             await userRepository.UpdateAsync(updateUser, cancellationToken);
-
             return Ok();
         }
 
         [HttpDelete]
-        public async Task<ActionResult> Delete(int id, CancellationToken cancellationToken)
+        public async Task<ApiResult> Delete(int id, CancellationToken cancellationToken)
         {
             var user = await userRepository.GetByIdAsync(CancellationToken.None, id);
             await userRepository.DeleteAsync(user, cancellationToken);
